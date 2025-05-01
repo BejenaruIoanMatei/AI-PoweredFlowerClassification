c90eb20f16a67b7482b402f2177aa0564af8d4b7
BejenaruIoanMatei <bejematei@gmail.com>
2025-05-01 15:02:19 +0300

    4. Post model and templates implementation

--- licenta_project/licenta_project/urls.py
+++ licenta_project/licenta_project/urls.py
@@ -23,6 +23,7 @@ from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
+    path('', include('blog.urls')),
     path('admin/', admin.site.urls),
     path('register/', user_views.register, name='register'),
     path('profile/', user_views.profile, name='profile'),
