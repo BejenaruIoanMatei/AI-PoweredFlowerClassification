e05e739f4f0283f6729542001c28c81cc138a8a6
BejenaruIoanMatei <bejematei@gmail.com>
2025-05-03 19:28:26 +0300

    12. Flowers integration into the garden

--- licenta_project/virtual_garden/views.py
+++ licenta_project/virtual_garden/views.py
@@ -4,14 +4,11 @@ from .models import VirtualGarden, GardenFlower
 
 class VirtualGardenView(LoginRequiredMixin, TemplateView):
     template_name = 'virtual_garden/virtual_garden.html'
+    context_object_name = 'virtual_garden'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-
-        # Creează grădina dacă nu există deja
         garden, created = VirtualGarden.objects.get_or_create(user=self.request.user)
-
-        # Adaugă grădina și florile din grădină în context
         context['garden'] = garden
         context['garden_flowers'] = GardenFlower.objects.filter(garden=garden).select_related('flower')
         return context
