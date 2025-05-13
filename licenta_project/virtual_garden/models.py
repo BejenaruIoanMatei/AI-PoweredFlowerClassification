3887463e8c3cbb8849150088ab18fc6ba8962e3b
BejenaruIoanMatei <bejematei@gmail.com>
2025-05-13 15:36:54 +0300

    21. Updated 'Flowers' page to display additional info

--- licenta_project/virtual_garden/models.py
+++ licenta_project/virtual_garden/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 class Flower(models.Model):
     name = models.CharField(max_length=100, unique=True)
     icon_path = models.CharField(max_length=200)
+    description = models.TextField(blank=True, null=True)
     
     def __str__(self):
         return self.name
