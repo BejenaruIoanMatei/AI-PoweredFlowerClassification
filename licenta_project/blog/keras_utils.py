6f1ab97994688474109f021dd4b0a200f9a48fb1
BejenaruIoanMatei <bejematei@gmail.com>
2025-06-02 18:32:05 +0300

    26: New version of the model + UI improvements

--- licenta_project/blog/keras_utils.py
+++ licenta_project/blog/keras_utils.py
@@ -5,10 +5,10 @@ from keras.models import load_model
 import os
 import json
 
-model_path = '/Users/user/Documents/GitHub/LucrareLicenta-FII-UAIC/classifier/1-iunie/Modul-1-iunie.keras'
+model_path = '/Users/user/Documents/GitHub/LucrareLicenta-FII-UAIC/classifier/2-iunie/Other versions/Modul-2-iunie-Adam.keras'
 model = load_model(model_path)
 
-with open('/Users/user/Documents/GitHub/LucrareLicenta-FII-UAIC/classifier/1-iunie/class_labels.json') as f:
+with open('/Users/user/Documents/GitHub/LucrareLicenta-FII-UAIC/classifier/2-iunie/class_labels.json') as f:
     class_labels = json.load(f)
 
 def classify_image(img_path):
